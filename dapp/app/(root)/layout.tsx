bba06f658f3117d3918cbfe49e95cad1a2c116c6
Prince Eddie <igberaeseedward2005@gmail.com>
2025-07-03 03:56:24 -0700

    july 2nd login logic atempt-2

--- dapp/app/(root)/layout.tsx
+++ dapp/app/(root)/layout.tsx
@@ -1,16 +1,22 @@
-"use client"
-import Header from "@/components/Header"
-import { SessionProvider } from 'next-auth/react';
+"use client";
 
+import Header from "@/components/Header";
+import { SessionProvider } from "next-auth/react";
 
-const layout = ({children,session}: { children: React.ReactNode,session:any }) => {
+const RootLayout = ({
+  children,
+  session,
+}: {
+  children: React.ReactNode;
+  session: any;
+}) => {
   return (
     <SessionProvider session={session}>
-    <div className='min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50'>
+      <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50">
         {children}
-    </div>
+      </div>
     </SessionProvider>
-  )
-}
+  );
+};
 
-export default layout
+export default RootLayout;

ab80ffe58fd878b32d287957dea4821885ac3a48
Prince Eddie <igberaeseedward2005@gmail.com>
2025-07-14 04:06:39 -0700

    my commit

--- dapp/app/(root)/layout.tsx
+++ dapp/app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/lib/auth"; 
 import AuthProvider from "@/components/authProvider"; 
+import Summarizer from "@/components/user/Summarizer";
 export default async function RootLayout({
   children,
 }: {
@@ -12,9 +13,13 @@ export default async function RootLayout({
   return (
         
         <AuthProvider session={session}>
-          <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50">
-           
+          <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-purple-50">  
             {children}
+            {!session?(
+              <div></div>
+            ):(
+              <Summarizer/>
+            )}
           </div>
         </AuthProvider>
       
